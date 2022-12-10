a6c99cabca79ddeb45bd29a9c1338d4128c6a37d
dome <2789448528@qq.com>
2022-12-10 11:20:30 +0800

    作业

--- 12.10 后台/src/views/Categories.vue
+++ 12.10 后台/src/views/Categories.vue
@@ -1,126 +1,234 @@
 <template>
-	<div>
-		<el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/Index' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-    </el-breadcrumb>
-	 <el-button type="primary" >添加分类</el-button>
-	  <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column type="expand">
-      <template slot-scope="props">
-        <el-form label-position="left" inline class="demo-table-expand">
-          <el-form-item label="商品名称">
-            <span>{{ props.row.cat_name }}</span>
-          </el-form-item>
-          <el-form-item label="所属店铺">
-            <span>{{ props.row.shop }}</span>
-          </el-form-item>
-          <el-form-item label="商品 ID">
-            <span>{{ props.row.id }}</span>
-          </el-form-item>
-          <el-form-item label="店铺 ID">
-            <span>{{ props.row.shopId }}</span>
-          </el-form-item>
-          <el-form-item label="商品分类">
-            <span>{{ props.row.category }}</span>
-          </el-form-item>
-          <el-form-item label="店铺地址">
-            <span>{{ props.row.address }}</span>
-          </el-form-item>
-          <el-form-item label="商品描述">
-            <span>{{ props.row.desc }}</span>
-          </el-form-item>
-        </el-form>
-      </template>
-    </el-table-column>
-	 <el-table-column
-      label="#"
-      type="index">
-    </el-table-column>
-    <el-table-column
-      label="分类名称"
-      prop="cat_name">
-    </el-table-column>
-    <el-table-column
-      label="是否有效"
-      prop="cat_deleted">
-	   <el-table-column prop="cat_deleted" label="等级">
-				<template slot-scope="scope">
-          <el-button v-if="scope.row.cat_deleted == true">×</el-button>
-          <el-button type="success" v-if="scope.row.cat_deleted == false">√</el-button>
+  <div class="categories">
+    <div class="categories-top">
+      <el-button type="primary" @click="sortadd">添加分类</el-button>
+    </div>
+    <!-- 头部添加按钮 -->
+    <el-table
+      :data="list"
+      style="width: 100%; margin-bottom: 20px"
+      row-key="cat_id"
+      border
+      default-expand-all
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+    >
+      <el-table-column type="index" label="#" width="60"> </el-table-column>
+      <el-table-column prop="cat_name" label="分类名称" width="280">
+      </el-table-column>
+      <el-table-column prop="cat_deleted" label="是否有效">
+        <template>
+          <i class="el-icon-circle-check"></i>
         </template>
-				 </el-table-column>
-    </el-table-column>
-
-    <el-table-column
-      label="排序"
-      prop="cat_level">
-	   <el-table-column prop="cat_level" label="等级">
-				<template slot-scope="scope">
-          <el-button v-if="scope.row.cat_level == 0">一级</el-button>
-          <el-button type="success" v-if="scope.row.cat_level == 1">二级</el-button>
-          <el-button type="info" v-if="scope.row.cat_level == 2">三级</el-button>
-        </template>
-				 </el-table-column>
-    </el-table-column>
-	<el-table-column label="操作">
+      </el-table-column>
+      <el-table-column prop="cat_level" label="排序">
         <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+          <div v-show="scope.row.cat_level == 0" class="box">一级</div>
+          <div v-show="scope.row.cat_level == 1" class="box1">二级</div>
+          <div v-show="scope.row.cat_level == 2" class="box2">三级</div>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            class="el-icon-edit"
+            type="primary"
+            @click="edit(scope.row)"
             >编辑</el-button
           >
-          <el-button size="mini" type="danger" @click="del(scope.row.id)"
+          <el-button
+            size="mini"
+            type="danger"
+            class="el-icon-delete"
+            @click="del(scope.row.cat_id)"
             >删除</el-button
           >
         </template>
       </el-table-column>
-  </el-table>
-	
- </div>
+    </el-table>
+    <!-- 表格 -->
+
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="page.pagenum"
+      :page-sizes="[1, 2, 5, 10]"
+      :page-size="page.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total"
+      style="margin-top: 15px"
+    >
+    </el-pagination>
+    <!-- 分页 -->
+
+    <el-dialog :title="title" :visible.sync="dialogVisible" width="50%">
+      <el-form ref="form" :model="form" label-width="80px" :rules="rules">
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="form.cat_name"></el-input>
+        </el-form-item>
+
+        <el-form-item label="父级分类" v-show="title == '添加商品分类'">
+          <el-cascader
+            :options="list"
+            clearable
+            change-on-select
+            :props="defaultParams"
+            @change="handleChange"
+            ref="cascader"
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="qx">取 消</el-button>
+        <el-button type="primary" @click="sortqd">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 添加编辑对话框 -->
+  </div>
 </template>
 
 <script>
 export default {
-	data () {
-		return {
-			tableData: [],
-			cat:{
-				type:3,
-				pagenum:1,
-				pagesize:5
-			},
-			id:0,
-			attrid:0,
-		}
-	},
-	methods: {
-		 fn() {
-      this.$http.get("/api/categories", { params: this.cat }).then((res) => {
+  data() {
+    return {
+      defaultParams: {
+        label: "cat_name",
+        value: "cat_name",
+        children: "children"
+      }, //级联选择器展示内容
+      page: {
+        pagenum: 1,
+        pagesize: 5
+      }, //分页
+      list: [], //数据渲染
+      total: 0, //总条数
+      title: "添加商品分类", //动态模态框标题
+      dialogVisible: false, //添加删除模态框
+      id: 0, //编辑拿到当前id
+      form: {
+        cat_name: "",
+        cat_level: "",
+        cat_pid: ""
+      },
+      rules: {
+        cat_name: [
+          { required: true, message: "分类名称不能为空", trigger: "blur" }
+        ]
+      } //表单的正则
+    }
+  },
+  methods: {
+    qx() {
+      this.dialogVisible = false
+      this.form.cat_name = ""
+    },
+
+    handleSizeChange(val) {
+      this.page.pagesize = val
+      this.render()
+
+      // console.log(val)
+    },
+    handleCurrentChange(val) {
+      // console.log(val)
+      this.page.pagenum = val
+      this.render()
+    },
+    fn() {
+      this.$http({
+        method: "get",
+        url: `api/categories`,
+        params: this.page
+      }).then((res) => {
         console.log(res)
-				this.tableData=res.data.data.result
+        if (res.data.meta.status == 200) {
+          this.list = res.data.data.result
+          this.total = res.data.data.total
+        }
       })
     },
-		del(){
-			this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+    //封装渲染页面接口
+    sortadd() {
+      this.dialogVisible = true
+      this.title = "添加商品分类"
+    },
+    // 添加按钮
+    handleChange() {
+      ;(this.form.cat_level =
+        this.$refs["cascader"].getCheckedNodes()[0].data.cat_level + 1),
+        (this.form.cat_pid =
+          this.$refs["cascader"].getCheckedNodes()[0].data.cat_id)
+    },
+
+    sortqd() {
+      if (this.title == "添加商品分类") {
+        this.$http({
+          method: "post",
+          url: "api/categories",
+          data: this.form
+        }).then((res) => {
+          // console.log(res)
+          if (res.data.meta.status == 201) {
+            this.dialogVisible = false
+            this.$message({
+              message: "添加商品分类名称成功",
+              type: "success"
+            })
+            this.fn()
+            this.form.cat_name = ""
+          }
+        })
+      } else {
+        this.$http({
+          method: "put",
+          url: `api/categories/${this.id}`,
+          data: this.form
+        }).then((res) => {
+          // console.log(res)
+          this.dialogVisible = false
+
+          this.$message({
+            message: "更新商品分类名称成功",
+            type: "success"
+          })
+          this.form.cat_name = ""
+          this.fn()
+        })
+      }
+    },
+    // 确定按钮
+    edit(row) {
+      // console.log(row)
+      this.title = "修改分类"
+      this.id = row.cat_id
+      this.dialogVisible = true
+      this.form.cat_name = row.cat_name
+    },
+    // 编辑
+    del(id) {
+      // console.log(id)
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
       })
         .then(() => {
-          // 点击确定
-          this.$http({ method: "delete" ,url: `/api/categories/${this.id}/attributes/${this.attrid}`}).then((res) => {
-            console.log(res)
-            if (res.data.meta.status == 200) {
-              this.$message({
-                message: res.data.meta.msg,
-                type: 'success'
-              });
-              //   重新获取用户列表，渲染页面
+          this.$http({
+            method: "delete",
+            url: `api/categories/${id}`
+          }).then((res) => {
+            {
+              // console.log(res)
+              if (res.data.meta.status == 200) {
+                this.fn()
+                // 删除调用渲染
+              }
             }
           })
-          this.fn()
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          })
         })
         .catch(() => {
           this.$message({
@@ -128,37 +236,60 @@ export default {
             message: "已取消删除"
           })
         })
-		}
-	},
-	created () {
-		this.fn()
-	},
-	mounted () {
-
-	},
-	components: {
-
-	},
-	computed: {
-
-	},
-	watch: {
-
-	},
+    }
+    // 删除
+  },
+  created() {
+    this.fn()
+  },
+  mounted() {},
+  components: {},
+  computed: {},
+  watch: {}
 }
 </script>
 
-<style lang='scss' scoped>
-.demo-table-expand {
-  font-size: 0;
+<style lang="scss" scoped>
+.categories {
+  border: 1px solid #ebeef5;
+  background-color: #fff;
+  color: #303133;
+  -webkit-transition: 0.3s;
+  transition: 0.3s;
+  padding: 20px;
 }
-.demo-table-expand label {
-  width: 90px;
-  color: #99a9bf;
+.categories-top {
+  display: flex;
 }
-.demo-table-expand .el-form-item {
-  margin-right: 0;
-  margin-bottom: 0;
-  width: 50%;
+.el-icon-circle-check {
+  background: #39ce39;
+  border-radius: 50%;
+}
+.box {
+  width: 40px;
+  height: 20px;
+  border-radius: 5%;
+  line-height: 20px;
+  text-align: center;
+  background: #409eff;
+  color: #fff;
+}
+.box1 {
+  width: 40px;
+  height: 20px;
+  border-radius: 5%;
+  line-height: 20px;
+  text-align: center;
+  background: #67c23a;
+  color: #fff;
+}
+.box2 {
+  width: 40px;
+  height: 20px;
+  border-radius: 5%;
+  line-height: 20px;
+  text-align: center;
+  background: #e6a23c;
+  color: #fff;
 }
 </style>
